a289ba2e5c5664b409d43eda0f3c7a7d11f67659
Kwon Valerii <wellko24@gmail.com>
2022-11-11 12:32:15 +0600

    added all components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserForm from "./Components/UserForm/UserForm";
+import Users from "./Components/Users/Users";
+import {User} from "./types";
 
 function App() {
+	const[users, setUsers] = useState<User[]>([])
+
+	const addUser = (user:User) => {
+		setUsers(prev => [...prev, user])
+	}
+
+
 	return (
 		<div className='container-fluid'>
-			<UserForm/>
+			<div className='row'>
+				<div className='col-6 p-5 border border-dark border-2'><UserForm onSubmit={addUser}/></div>
+				<div className='col-6'><Users users={users}/></div>
+			</div>
 		</div>
 	);
 }
